ca200436e657a749dbba46bf410f64a788d0c766
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-11-05 19:24:26 +0900

    Carthage対応

---
+++ Carthage/ARKitRecorder/ARKitRecorder/ARKitRecorder.h
@@ -0,0 +1,19 @@
+//
+//  ARKitRecorder.h
+//  ARKitRecorder
+//
+//  Created by Tomoya Hirano on 2017/11/05.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKitRecorder.
+FOUNDATION_EXPORT double ARKitRecorderVersionNumber;
+
+//! Project version string for ARKitRecorder.
+FOUNDATION_EXPORT const unsigned char ARKitRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKitRecorder/PublicHeader.h>
+
+
